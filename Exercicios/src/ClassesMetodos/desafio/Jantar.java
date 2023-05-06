cb8bc315fdb5dd3d6dae33c281befd257a040e5e
TANAKMH <matheushstanaka@gmail.com>
2023-05-06 19:54:56 -0300

    Create Jantar.java

---
+++ Exercicios/src/ClassesMetodos/desafio/Jantar.java
@@ -0,0 +1,21 @@
+package ClassesMetodos.desafio;
+
+public class Jantar {
+	public static void main(String[] args) {
+		
+		Comida c1 = new Comida("Arroz", 0.223);
+		Comida c2 = new Comida("Feijao", 0.300);
+		
+		Pessoa p = new Pessoa("João", 99.8);
+		
+		System.out.println(p.apresentar());
+		
+		p.comer(c1);
+
+		System.out.println(p.apresentar());
+		p.comer(c2);
+		
+		System.out.println(p.apresentar());
+	}
+
+}
