1582c0ba994a279f4fff367233c82c9d77b5200a
TANAKMH <matheushstanaka@gmail.com>
2023-05-06 19:46:14 -0300

    Create DesafioIF.java

---
+++ Exercicios/src/EstruturasDeControle/DesafioIF.java
@@ -0,0 +1,15 @@
+package EstruturasDeControle;
+
+public class DesafioIF {
+
+	public static void main(String[] args) {
+		double nota = 1.3;
+
+		// nao usar; em estrutura de contole (tem um exceção)
+		if (nota >= 9.0){
+			System.out.println("Quadro de Honra!");
+			System.out.println("Você é fera!!!");
+		}
+	}
+
+}
