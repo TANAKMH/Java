11041fe5ce2843968c5551192a100bb3ef29e571
TANAKMH <matheushstanaka@gmail.com>
2023-05-06 19:43:44 -0300

    Create Inferencia.java

---
+++ Exercicios/src/Fundamentos/Inferencia.java
@@ -0,0 +1,36 @@
+package Fundamentos;
+
+public class Inferencia {
+	public static void main(String[] args) {
+//'var' pode ser usada com numeros flutuantes, inteiros e strings
+
+		
+		double a = 4.5; // declaração e inicialização
+		System.out.println(a);
+		
+		a = 12;
+		System.out.println(a);
+		
+		var m = 80;
+		System.out.println(m);
+		
+		var b = 4.5;
+		System.out.println(b);
+		
+		var c = "Texto";
+		System.out.println(c);
+		c = "Outro texto";
+		System.out.println(c);
+		
+		double d; // variavel foi declarada 
+		d = 123.65; // variavel foi inicializada
+		System.out.println(d);//usada !!
+		
+		double e = 12;
+		e = 58;
+		System.out.println(e);
+		
+		
+	}
+
+} 
