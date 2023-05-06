be8638c2735f9394d93fd30f8b7f50eb48686c39
TANAKMH <matheushstanaka@gmail.com>
2023-05-06 19:42:44 -0300

    Create IF.java

---
+++ Exercicios/src/EstruturasDeControle/IF.java
@@ -0,0 +1,33 @@
+package EstruturasDeControle;
+
+import java.util.Scanner;
+
+public class IF {
+	
+	public static void main(String[] args) {
+		
+		
+		Scanner entrada = new Scanner(System.in);
+		
+		System.out.print("Infome a média: ");
+		double media = entrada.nextDouble();
+		
+		if(media <= 10 && media >= 7.0) {
+			System.out.println("Aprovado!"); 
+			System.out.println("Parabéns!");
+		}
+		
+		if(media < 7 && media >= 4.5) { 
+			System.out.println("Recuperação");
+		}
+		
+		boolean criterioReprovacaoAtingido = 
+				media < 4.5 && media >= 0;
+		if(criterioReprovacaoAtingido) { 
+			System.out.println("Reprovado");
+		}
+		
+		entrada.close();
+	}
+
+}
