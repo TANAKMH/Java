ff5252d82b6a022e9cff0c7c9e843b6d7cf907ac
TANAKMH <matheushstanaka@gmail.com>
2023-05-06 19:55:16 -0300

    Create PrimeiroTrauma.java

---
+++ Exercicios/src/ClassesMetodos/PrimeiroTrauma.java
@@ -0,0 +1,16 @@
+package ClassesMetodos;
+
+public class PrimeiroTrauma {
+
+	int a = 3;
+	static int b = 3;
+	
+	public static void main(String[] args) {
+		
+		PrimeiroTrauma t1 = new PrimeiroTrauma();
+		System.out.println(t1.a);
+		//ou
+		System.out.println(b);
+	}
+
+}
