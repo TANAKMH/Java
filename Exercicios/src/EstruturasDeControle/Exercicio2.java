ad2f158f6fd785a3fcda1cea26b98013a0fd9c41
TANAKMH <matheushstanaka@gmail.com>
2023-05-06 19:47:52 -0300

    Create Exercicio2.java

---
+++ Exercicios/src/EstruturasDeControle/Exercicio2.java
@@ -0,0 +1,26 @@
+package EstruturasDeControle;
+
+import java.util.Scanner;
+
+public class Exercicio2 {
+	public static void main(String[] args) {
+		/*Criar um programa informa se o ano atual é um ano 
+		 * bissexto;
+		 */
+		Scanner entrada = new Scanner(System.in);
+		System.out.println("Digite o ano:");
+		int ano = entrada.nextInt();
+		
+		int bissexto = ano % 4;
+		
+		if(bissexto == 0) {
+			System.out.println(ano + "é um ano bissexto");
+		}else {
+			System.out.println(ano + "não é um ano bissexto" );
+		}
+		
+
+		entrada.close();
+	}
+
+}
