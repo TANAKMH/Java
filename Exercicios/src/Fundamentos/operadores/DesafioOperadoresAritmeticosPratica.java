c764d9b0d221c42a5ce794fec2b2f8505f17bc1c
TANAKMH <matheushstanaka@gmail.com>
2023-05-06 19:41:46 -0300

    Create DesafioOperadoresAritmeticosPratica.java

---
+++ Exercicios/src/Fundamentos/operadores/DesafioOperadoresAritmeticosPratica.java
@@ -0,0 +1,15 @@
+package Fundamentos.operadores;
+
+public class DesafioOperadoresAritmeticosPratica {
+	
+	public static void main(String[] args) {
+		
+		int a = 3 * 4 - 10;
+		int b = (int)Math.pow(a, 3);
+		double c = Math.pow(a, 3);
+		
+		System.out.println(b);
+		System.out.println(c);
+	}
+
+}
