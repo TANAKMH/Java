2d796a7a7d35989bbd13e551051215888332cebb
TANAKMH <matheushstanaka@gmail.com>
2023-05-06 19:42:40 -0300

    Create For3.java

---
+++ Exercicios/src/EstruturasDeControle/For3.java
@@ -0,0 +1,34 @@
+package EstruturasDeControle;
+
+public class For3 {
+	
+	public static void main(String[] args) {
+		
+		/*int i = 0;
+		for(; i < 10; i ++) {
+			System.out.println(i);
+		}
+		System.out.println("Saiu do for...");
+		System.out.println(i);*/
+		
+		/*for(int i = 0; i < 10; i ++) {
+			System.out.println(i);
+			
+		}
+		int i = 0;
+		System.out.println("Saiu do for...");
+		System.out.println(i);*/
+		
+		
+		
+		
+		// Um laço dentro do outro
+		for(int i = 0; i < 10; i ++) {
+			for(int j = 0; j < 10; j ++) {
+				System.out.printf("[%d %d]", i , j);
+			}
+			System.out.println();
+		}
+	}
+
+}
