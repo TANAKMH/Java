2f0aff39e19b95d3c2e5100f2a4d9e3d0b264287
TANAKMH <matheushstanaka@gmail.com>
2023-05-06 19:46:47 -0300

    Create Continue1.java

---
+++ Exercicios/src/EstruturasDeControle/Continue1.java
@@ -0,0 +1,20 @@
+package EstruturasDeControle;
+
+public class Continue1 {
+	
+	public static void main(String[] args) {
+		
+		for (int i = 0; i < 10; i++) {
+			if(i % 2 == 1) {
+				continue;
+			}
+			
+			System.out.println(i);
+		}
+		
+		for (int i = 0; i <= 10; i++) {
+			if(i == 5) continue;
+			System.out.println(i);
+		}
+	}
+}
