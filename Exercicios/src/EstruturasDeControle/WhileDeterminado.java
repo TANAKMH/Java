9bd1d23bc39b2403211bd98a795a96c293c4ec89
TANAKMH <matheushstanaka@gmail.com>
2023-05-06 19:43:13 -0300

    Create WhileDeterminado.java

---
+++ Exercicios/src/EstruturasDeControle/WhileDeterminado.java
@@ -0,0 +1,14 @@
+ package EstruturasDeControle;
+
+public class WhileDeterminado {
+
+	public static void main(String[] args) {
+		
+		int contador = 0;
+		while(contador <= 20) {
+			System.out.printf("i = %d\n", contador);
+			contador ++; // contador += 2;
+			
+		}
+	}
+}
