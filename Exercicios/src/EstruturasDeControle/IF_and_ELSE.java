6c3bca84bcc33101043f454aef1985873d4e3478
TANAKMH <matheushstanaka@gmail.com>
2023-05-06 19:42:48 -0300

    Create IF_and_ELSE.java

---
+++ Exercicios/src/EstruturasDeControle/IF_and_ELSE.java
@@ -0,0 +1,18 @@
+package EstruturasDeControle;
+
+import javax.swing.JOptionPane;
+
+public class IF_and_ELSE {
+	public static void main(String[] args) {
+
+		String valor = JOptionPane.showInputDialog("Informe o número:");
+		int numero = Integer.parseInt(valor);
+		
+		if (numero % 2 == 0) {
+			System.out.println("Número par!");
+			} else {
+			System.out.println("Número ímpar!");
+		}
+	}
+
+}
