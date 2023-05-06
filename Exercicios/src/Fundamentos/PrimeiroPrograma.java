2cc049f0a547c6b1e3bb70cdcfbba2811da38816
TANAKMH <matheushstanaka@gmail.com>
2023-05-06 19:41:19 -0300

    Create PrimeiroPrograma.java

---
+++ Exercicios/src/Fundamentos/PrimeiroPrograma.java
@@ -0,0 +1,29 @@
+package Fundamentos;
+/**
+ * Essa classe representa... 
+ * 
+ * @author Matheus Tanaka <email.com>
+ * @since JDK1.0
+ */
+public class PrimeiroPrograma {
+	
+	/**
+	 * 
+	 * @param args
+	 * 
+	 */
+	public static void main(String[] args) {
+		
+		// Uma sentença de código termina com ;
+		System.out.println("Primeiro Programa Parte #01");
+		System.out.println("Primeiro Programa Parte #01");
+		/* 
+		 
+		 Linha 1
+		 Linha 2
+		 
+		 */
+		System.out.println("Primeiro programa Parte #03!");
+		System.out.println("Fim");
+	}
+}
