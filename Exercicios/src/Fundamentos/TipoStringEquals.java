86ff7e20c51c1877e8ac1ce2edd26173af9226fb
TANAKMH <matheushstanaka@gmail.com>
2023-05-06 19:40:56 -0300

    Create TipoStringEquals.java

---
+++ Exercicios/src/Fundamentos/TipoStringEquals.java
@@ -0,0 +1,28 @@
+package Fundamentos;
+
+import java.util.Scanner;
+
+public class TipoStringEquals {
+	
+	public static void main(String[] args) {
+		
+		
+		System.out.println("2" == "2");
+		
+		String s = new String("2");
+		System.out.println("2" == s);
+		System.out.println("2".equals(s));
+		
+		Scanner entrada= new Scanner(System.in);
+		
+		String s2 = entrada.nextLine();
+		System.out.println("2" == s2.trim());
+		System.out.println("2".equals(s2.trim()));// com nextLine ele fica vdd com.trim
+		
+		entrada.close();
+		
+		// .trim : vai retirar os espaços em branco
+		// quando vc estiver comparando string usa .equals
+	}
+
+}
