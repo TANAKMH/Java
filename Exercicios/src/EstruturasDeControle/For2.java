2d5c1f1e996b5ffc65a44389c3270ad99711caa6
TANAKMH <matheushstanaka@gmail.com>
2023-05-06 19:42:36 -0300

    Create For2.java

---
+++ Exercicios/src/EstruturasDeControle/For2.java
@@ -0,0 +1,11 @@
+package EstruturasDeControle;
+
+public class For2 {
+	public static void main(String[] args) {
+		
+		for(int contador = 10; contador >= 0 ; contador -= 2) {
+			System.out.printf(" i = %d\n", contador);
+		}
+	}
+
+}
